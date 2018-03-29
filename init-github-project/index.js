86e9ab0416cea26fcc38c4e39e5d59a8787747f9
zhiwei_zhai <zhiwei_zhai@kingdee.com>
2018-03-29 16:57:15 +0800

    add-new

---
+++ init-github-project/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+
+exec('git clone <project address>', (err) => {
+    if(err){
+        console.log(err)
+    }
+
+    exec('cd <project name> && <install package(npm i or cnpm i or yarn)>', (err) => {
+        if(err)console.log(err);
+         console.log('ok')
+    })
+})
